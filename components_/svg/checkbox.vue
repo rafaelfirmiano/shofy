59e0c6f160a290f122f6ad9e36c2d6f24c3e7d58
rafael firmiano <rafael.firmiano@tcmsvc.com>
2024-04-29 14:18:09 -0300

    Deleted folders

--- components_/svg/checkbox.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-    <path d="M10 16.9L6 12.9L7.4 11.5L10 14.1L16.6 7.5L18 8.9L10 16.9Z" fill="white"/>
-  </svg>
-</template>
