0dd204c9204065488e0c364d2bf7f79deabdeb4a
rafael firmiano <rafael.firmiano@tcmsvc.com>
2024-05-13 10:23:14 -0300

    Fixing image issue

--- pages/details.vue
+++ pages/details.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <img src="/img/custom/Details.png" />
+
   </div>
 </template>
 <style scoped>
-div img {
-  width: 100%;
-}
+
 </style>
 <script setup lang="ts">
 </script>
